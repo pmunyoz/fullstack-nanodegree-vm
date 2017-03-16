38366e2edd2df77f3417cdb49d86279460a118bc
Pascual Muñoz Martinez <pascual.munoz@azkar.com>
2017-03-16 16:11:32 +0100

    final

--- vagrant/Ejemplos/4/views.py
+++ vagrant/Ejemplos/4/views.py
@@ -1,10 +1,13 @@
 from models import Base, User
-from flask import Flask, jsonify, request, url_for, abort
+from flask import Flask, request, jsonify, url_for, abort, g
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.exc import MultipleResultsFound
+
+from flask.ext.httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
 
 engine = create_engine('sqlite:///users.db')
 
@@ -12,6 +15,13 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
+
+@auth.verify_password
+def verify_password(username, password):
+    user = session.query(User).filter_by(username = username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user    
 
 @app.route('/api/users', methods = ['POST', 'GET'])
 def new_user():
@@ -26,17 +36,16 @@ def new_user():
 
         user = User(username = username)
         user.hash_password(password)
-        print (user.password_hash)
-
+        
         session.add(user)
         session.commit()    
         return jsonify({'username': user.username}), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
     elif request.method == 'GET':
         users = session.query(User).all()
         return jsonify(users = [i.username for i in users])
-        
+
 @app.route('/api/users/<int:id>')
-# def get_user(id):
+def get_user(id):
     try:
         user = session.query(User).filter_by(id=id).one()
         if not user:
@@ -48,8 +57,14 @@ def new_user():
         print e
         abort(400)
 
+    g.user = user;
     return jsonify({'username': user.username})
     
+@app.route('/api/protected_resource')
+@auth.login_required
+def get_resource():
+    return jsonify({ 'data': 'Hello, %s!' % g.user.username })
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8080)
